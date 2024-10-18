314825779fe1e0049dcb187519dc81d018001ee2
Carmine Paolino <carmine@paolino.me>
2024-10-18 14:03:21 +0200

    Initial Commit

---
+++ Cluster Headache TrackerTests/Cluster_Headache_TrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  Cluster_Headache_TrackerTests.swift
+//  Cluster Headache TrackerTests
+//
+//  Created by Carmine Paolino on 18.10.24.
+//
+
+import Testing
+@testable import Cluster_Headache_Tracker
+
+struct Cluster_Headache_TrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
